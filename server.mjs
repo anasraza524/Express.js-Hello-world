0df69040cfc47f2a54fca3f4c0e4d1edec26f943
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-12-06 22:52:34 +0500

    weather web with server

--- server.mjs
+++ server.mjs
@@ -1,12 +1,56 @@
 import express from 'express'
+import path from 'path';
+import cors from 'cors'
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
+app.use(cors());
+app.use(express.json())
+// url params:
+// http://localhost:5001/weather/karachi/75600
+// app.get('/weather/:cityName/:zip', (req, res) => {
+
+
+// query parameters:
+// http://localhost:5001/weather?zip=75600&city=karachi
+// app.get('/weather', (req, res) => {
+//       console.log(req.query.city)
+//       console.log(req.query.zip)
+
+// body:
+// http://localhost:5001/weather
+// {
+//   cityName: "karachi",
+//   zip: 75600
+// }
+
+// app.use(express.json());
+// app.get('/weather', (req, res) => {
+//       console.log(req.body.cityName)
+//       console.log(req.body.zip)
+//
+// parsing body is must on server
+
+
+app.get('/weather/:cityName',(req,res)=>{
+console.log('params',req.params.cityName)
+
+  res.send({
+        city: req.params.cityName,
+        temp: 30,
+        min: 26,
+        max: 31,
+        humidity: 72,
+        serverTime: new Date().toString()
+  })
+})
 app.get('/', (req, res) => {
     console.log('requsret come from:' + new Date().toString())
   res.send('Respone From Anas Raza!')
 })
-
+const dir_name = path.resolve()
+app.use('/',express.static(path.json(dir_name,'./web/build')))
+app.use('*',express.static(path.json(dir_name,'./web/build')))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
